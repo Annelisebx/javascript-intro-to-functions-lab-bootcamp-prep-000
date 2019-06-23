7b21a8804734f1512f540b45d86f5a5fac331fb7
Annelise Blanchard <annelisebx@gmail.com>
2019-06-23 17:47:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,14 +24,3 @@ if (string.toUpperCase() === string) {
 }
 
 
-function sayHiToGrandma(string) {
-if (string.toLowerCase() === string) {
-    return "I can't hear you!";
-  }
-}
-function sayHiToGrandma(string) {
-if (string === "I love you, Grandma") {
-    return "I love you, too.";
-}
-}
- 
