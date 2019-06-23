765294babb7d8701a0365c6a638bf8d80d5a6726
Annelise Blanchard <annelisebx@gmail.com>
2019-06-23 17:33:33 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string) {
+  return string.toUpperCase ()
+}
+function whisper(string) {
+  return string.toLowerCase ()
+}
+function logShout(string) {
+  console.log (string.toUpperCase())
+}
+function logWhisper(string) {
+  console.log (string.toLowerCase())
+} 
+
+var uppercase = "HELLO!"
+
+var lowercase = "hello"
+
+var mixedcase = "Hello"
+
+function sayHiToGrandma(string) {
+  if (string.toUpperCase() === string)
+  return console.log("YES INDEED!")
+  
+  else (string.toLowerCase () === string)
+  return console.log ("I can't Hear You!")
+}
